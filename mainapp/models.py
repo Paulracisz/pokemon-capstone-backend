030192aaa908eed9d54a8bbe5b0fc00b4541189f
Keith Garcia <Keith.Garcia444@gmail.com>
2020-10-07 12:29:11 -0400

    basic models

--- mainapp/models.py
+++ mainapp/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Pokemon(models.Model):
+    poke_ball = models.IntegerField(default=0)
+    great_ball = models.IntegerField(default=0)
+    ultra_ball = models.IntegerField(default=0)
+    master_ball = models.IntegerField(default=0)
+    exp = models.IntegerField(default=0)
+    currency = models.IntegerField(default=0)
+    level = models.IntegerField(default=0)
+    
