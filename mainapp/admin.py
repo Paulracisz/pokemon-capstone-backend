03b3393f74ccbcfca5f4458c0e9ed8d5c8ca6942
Ruben Espino <56930001+r-o-e-574@users.noreply.github.com>
2020-10-07 13:59:32 -0500

    added pokemonusermodel and pokemonmodel.

--- mainapp/admin.py
+++ mainapp/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from mainapp.models import PokemonTrainer, Pokemon
 
 # Register your models here.
+admin.site.register(PokemonTrainer)
+admin.site.register(Pokemon)
--- mainapp/models.py
+++ mainapp/models.py
@@ -1,12 +1,51 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import AbstractUser
 
-
-class Pokemon(models.Model):
-    poke_ball = models.IntegerField(default=0)
+    
+class PokemonTrainer(AbstractUser):
+    displayname = models.CharField(max_length=80)
+    email = models.EmailField(blank=True, null=True)
+    bio = models.TextField(blank=True, null=True)
+    pokedexed = models.ManyToManyField('Pokemon', related_name="pokemon_indexed", symmetrical=False, blank=True)
+    poke_ball = models.IntegerField(default=5)
     great_ball = models.IntegerField(default=0)
     ultra_ball = models.IntegerField(default=0)
     master_ball = models.IntegerField(default=0)
     exp = models.IntegerField(default=0)
     currency = models.IntegerField(default=0)
     level = models.IntegerField(default=0)
+    
+class Pokemon(models.Model):
+    # Pokemon Name
+    name = models.CharField(max_length = 40)
+    # Images
+    front_normal_image = models.CharField(max_length = 100)
+    front_shiny_image = models.CharField(max_length = 100)
+    # Stats
+    hp = models.IntegerField()
+    attack = models.IntegerField()
+    defense = models.IntegerField()
+    special_attack = models.IntegerField()
+    special_defense = models.IntegerField()
+    speed = models.IntegerField()
+    # Abilities
+    ability_One = models.CharField(max_length = 40)
+    ability_Two = models.CharField(max_length = 40)
+    # Types
+    type_One = models.CharField(max_length = 40)
+    type_Two = models.CharField(max_length = 40)
+    # Base XP
+    base_experience = models.IntegerField()
+    #Default Pokemon?
+    is_default = models.BooleanField(default = 0)
+    # Leave a comment about your pokemon
+    your_thoughts = models.TextField(blank = True)
+    # Details about this pokemon can be found at:
+    details_at = models.URLField()
+    
+    def __str__(self):
+        return self.name
+    
+
     
