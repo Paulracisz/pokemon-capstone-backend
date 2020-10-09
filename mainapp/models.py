6a32c659ffb0127d637b71e8f7cdac70a06dd1ec
CoderJoe82 <coderjoe82@gmail.com>
2020-10-09 19:41:37 -0400

    testing for sanity

--- mainapp/models.py
+++ mainapp/models.py
@@ -2,17 +2,17 @@ from django.db import models
 
 
 class Pokemon(models.Model):
-    # Images
-    front_normal_image = models.CharField(max_length=100)
-    # Stats
-    height = models.IntegerField(default=0)
-    weight = models.IntegerField(default=0)
-    # Abilities
-    ability_One = models.CharField(max_length=40)
-    ability_Two = models.CharField(max_length=40)
-    ability_Three = models.CharField(max_length=40)
-    # Types
-    type_One = models.CharField(max_length=40)
-    type_Two = models.CharField(max_length=40)
-    # Base XP
-    base_experience = models.IntegerField(default=0)
+
+    name = models.CharField(max_length=40)
+    # front_normal_image = models.CharField(max_length=100)
+
+    # height = models.IntegerField(default=0)
+    # weight = models.IntegerField(default=0)
+
+    # ability_One = models.CharField(max_length=40)
+    # ability_Two = models.CharField(max_length=40)
+    # ability_Three = models.CharField(max_length=40)
+
+    # type_One = models.CharField(max_length=40)
+    # type_Two = models.CharField(max_length=40)
+    # base_experience = models.IntegerField(default=0)
