0563bcc6c4ea74a901d72330b5d0512e461798be
CoderJoe82 <coderjoe82@gmail.com>
2020-10-09 19:25:38 -0400

    database command finished

--- mainapp/models.py
+++ mainapp/models.py
@@ -2,7 +2,6 @@ from django.db import models
 
 
 class Pokemon(models.Model):
-    name = models.CharField(max_length=40)
     # Images
     front_normal_image = models.CharField(max_length=100)
     # Stats
