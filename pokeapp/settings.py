9f4d0d665daac7b7340ed397b6959c068f6d5adc
CoderJoe82 <coderjoe82@gmail.com>
2020-10-10 22:16:26 -0400

    removed unnecessary gitattributes file

--- pokeapp/settings.py
+++ pokeapp/settings.py
@@ -90,7 +90,31 @@ REST_FRAMEWORK = {
 }
 
 CORS_ALLOWED_ORIGINS = [
-    'http://localhost:3000'
+    'http://127.0.0.1:8000',
+    'http://localhost:3000',
+]
+
+
+
+CORS_ALLOW_METHODS = [
+    'DELETE',
+    'GET',
+    'OPTIONS',
+    'PATCH',
+    'POST',
+    'PUT',
+]
+
+CORS_ALLOW_HEADERS = [
+    'accept',
+    'accept-encoding',
+    'authorization',
+    'content-type',
+    'dnt',
+    'origin',
+    'user-agent',
+    'x-csrftoken',
+    'x-requested-with',
 ]
 
 CSRF_TRUSTED_ORIGINS = [
