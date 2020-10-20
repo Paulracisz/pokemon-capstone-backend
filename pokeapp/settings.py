09c646b4f8f13375c56c954a70a295a3a6846932
Ruben Espino <56930001+r-o-e-574@users.noreply.github.com>
2020-10-20 13:57:20 -0500

    put secret key in the .env gitignore db again

--- pokeapp/settings.py
+++ pokeapp/settings.py
@@ -11,16 +11,17 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 import os
-
+import environ 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
-
+ROOT_DIR = environ.Path(__file__) -2 
+env = environ.Env()
+env_file = ROOT_DIR('.env')
+env.read_env(env_file)
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
-
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'elbwb&ptgwo1jyca(jfhx6^-ra*#5+38@5fp+tf%@#-3$wm+_l'
+SECRET_KEY = env.str('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
